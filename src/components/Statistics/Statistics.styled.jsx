cd785031dd39aec5bf0df950eb084a8df13c172e
HOP3N <pankoff97@gmail.com>
2023-03-05 14:56:31 +0200

    22/stats

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -4,13 +4,13 @@ export const OptionsList = styled.ul`
   list-style: none;
 `;
 export const OptionsItem = styled.li`
-  font-size: 20px;
+  font-size: 24px;
   :not(:last-child) {
-    margin-bottom: 10px;
+    margin-bottom: 20px;
   }
 `;
 export const OptionsSpan = styled.span`
-  font-weight: 600;
+  font-weight: 400;
 `;
 export const OptionsTitle = styled.h2`
   text-align: center;
